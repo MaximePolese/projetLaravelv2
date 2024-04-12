a662e1efa51fcb03e2d00df855f7fcf32e51f334
maxime.polese <maxime.polese@le-campus-numerique.fr>
2024-04-12 11:04:03 +0200

    correction table

--- database/migrations/2024_04_11_080916_create_order_products_table.php
+++ database/migrations/2024_04_11_080916_create_order_products_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('order_product', function (Blueprint $table) {
+        Schema::create('order_products', function (Blueprint $table) {
             $table->foreignUuid('product_id')->references('id')->on('products');
             $table->foreignUuid('order_id')->references('id')->on('orders');
             $table->integer('quantity');
