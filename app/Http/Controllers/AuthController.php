de41c09aba937bbec92aa4b1a648570ff26e6d97
MaximePolese <maxime.polese@le-campus-numerique.fr>
2024-06-27 14:45:36 +0200

    retour login initial

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -40,6 +39,7 @@ class AuthController extends Controller
         ]);
 
         $token = $user->createToken('auth_token', ['*'], now()->addDays(1))->plainTextToken;
+
         return response()->json([
             'user' => $user,
             'access_token' => $token,
@@ -47,25 +47,7 @@ class AuthController extends Controller
         ]);
     }
 
-    public function authenticate(Request $request): RedirectResponse
-    {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
-
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-
-            return redirect()->intended('/');
-        }
-
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ])->onlyInput('email');
-    }
-
-    public function login(Request $request): JsonResponse
+    public function authenticate(Request $request): JsonResponse
     {
         if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
@@ -87,24 +69,48 @@ class AuthController extends Controller
         ]);
     }
 
+    public function logout(Request $request): JsonResponse
+    {
+        $user = $request->user();
+        if ($user->id == Auth::id()) {
+            $user->currentAccessToken()->delete();
+        }
+        return response()->json(['message' => 'Logged out']);
+    }
+
+//    public function authenticate(Request $request): JsonResponse
+//    {
+//        $credentials = $request->validate([
+//            'email' => ['required', 'email'],
+//            'password' => ['required'],
+//        ]);
+//
+//        if (Auth::attempt($credentials)) {
+//            $request->session()->regenerate();
+//            $request->user()->tokens()->delete();
+//            $request->user()->createToken('auth_token', ['*'], now()->addDays(1))->plainTextToken;
+//            return response()->json([
+//                'message' => 'User authenticated',
+//                'user' => $request->user(),
+//            ]);
+//        } else {
+//            return response()->json([
+//                'message' => 'Invalid credentials',
+//            ], 401);
+//        }
+//    }
+//
 //    public function logout(Request $request): JsonResponse
 //    {
-//        $user = $request->user();
-//        if ($user->id == Auth::id()) {
-//            $user->currentAccessToken()->delete();
+//        if ($request->user()->id == Auth::id()) {
+//            Auth::logout();
+//            $request->session()->invalidate();
+//            $request->session()->regenerateToken();
+//            $request->user()->tokens()->delete();
+//            return response()->json(['message' => 'Logged out']);
+//        } else {
+//            return response()->json(['message' => 'Unauthorized'], 401);
 //        }
-//        return response()->json(['message' => 'Logged out']);
 //    }
-
-    public function logout(Request $request): RedirectResponse
-    {
-        Auth::logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return redirect('/');
-    }
 }
 
