430121d64681f9497fbebb4e8eea85dc10746226
MaximePolese <maxime.polese@le-campus-numerique.fr>
2024-07-19 10:46:33 +0200

    Docker et DB fonctionnel

--- init.sql
+++
@@ -1,5 +0,0 @@
--- init.sql
-CREATE DATABASE IF NOT EXISTS craftcornerdata;
-CREATE USER 'admin'@'%' IDENTIFIED BY 'secret';
-GRANT ALL PRIVILEGES ON craftcornerdata.* TO 'admin'@'%';
-FLUSH PRIVILEGES;
